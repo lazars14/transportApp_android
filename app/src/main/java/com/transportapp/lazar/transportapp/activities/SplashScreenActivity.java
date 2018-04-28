6d45fbc5f930c9e0d17b474713578f79b428946b
Lazar Stijakovic <zomlaaa95@gmail.com>
2018-04-28 17:32:11 +0200

    Initial commit

---
+++ app/src/main/java/com/transportapp/lazar/transportapp/activities/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package com.transportapp.lazar.transportapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.transportapp.lazar.transportapp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+//        check internet connection
+//        check if logged in
+//        wait for one second
+//        if logged in main activity, or if not login activity
+
+//        wait for one second code
+//        try {
+//            sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+    }
+}
